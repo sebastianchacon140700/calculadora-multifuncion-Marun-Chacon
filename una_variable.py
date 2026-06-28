5ac1d77419482610ce18a49ffe4993b011160a28
susana marun <susana.patrcia.marun@gmail.com>
2026-06-28 19:37:24 -0300

    actualicacion. Orden y Comentarios

--- una_variable.py
+++ una_variable.py
@@ -3,10 +3,7 @@ from logicas.longitud import convertir_longitud
 from logicas.peso import convertir_peso
 from logicas.temperatura import convertir_temperatura
 from logicas.datos import convertir_datos
-from logicas.historial import (
-    leer_historial_una,
-    guardar_historial_una
-)
+from logicas.historial import (leer_historial_una,guardar_historial_una)
 
 class PanelUnaVariable(wx.Panel):
 
@@ -15,47 +12,36 @@ class PanelUnaVariable(wx.Panel):
         opcion = self.destino
         
         if self.destino == "":
-            self.resultado.SetLabel(
-            "Seleccione una unidad de destino"
-        )
+            self.resultado.SetLabel( "Selecciona una unidad de destino")
             return
 
         if self.origen == "":
-            self.resultado.SetLabel(
-            "Seleccione una unidad de origen"
-        )
+            self.resultado.SetLabel("Selecciona una unidad de origen")
             return
-
-        if opcion in {"Centimetros","Metros", "Kilometros","Pulgadas", "Pies","Yardas", "Millas"}:
-            resultado = convertir_longitud(
-                valor,
-                self.origen,
-                opcion
-            )
+    #-----------------------------------------
+    # Lógica de enrutamiento
+        if opcion in {"Centimetros",
+                      "Metros", 
+                      "Kilometros",
+                      "Pulgadas", 
+                      "Pies",
+                      "Yardas",
+                      "Millas" }:
+            resultado = convertir_longitud(valor, self.origen, opcion) 
 
         elif opcion in {
             "Gramos",
             "Kilogramos",
             "Libras",
             "Toneladas",
-            "Onzas"
-        }:
-            resultado = convertir_peso(
-                valor,
-                self.origen,
-                opcion
-            )
+            "Onzas"}:
+            resultado = convertir_peso(valor, self.origen, opcion) 
         
         elif opcion in{
             "Celsius",
             "Fahrenheit",
-            "Kelvin"
-        }:
-            resultado = convertir_temperatura(
-                valor,
-                self.origen,
-                opcion
-            )
+            "Kelvin"}:
+            resultado = convertir_temperatura(valor, self.origen, opcion) 
 
         elif opcion in{
             "Bits",
@@ -63,39 +49,26 @@ class PanelUnaVariable(wx.Panel):
             "Kilobytes",
             "Megabytes",
             "Gigabytes",
-            "Terabytes"
-        }:
-            resultado = convertir_datos(
-                valor,
-                self.origen,
-                opcion
-            ) 
-
+            "Terabytes"}:
+            resultado = convertir_datos(valor, self.origen, opcion) 
+    
         else:
             self.resultado.SetLabel("seleccione una opcion")
             return
-        self.resultado.SetLabel(
-            f"{resultado:g} {opcion}"
-        )
-
-        guardar_historial_una(
-            self.origen,
-            opcion,
-            valor,
-            resultado
-        )
+    #------------------------------------------
+    # Mostrar resultado y guardar en el historial
+        self.resultado.SetLabel(f"{resultado:g} {opcion}")
+        guardar_historial_una(self.origen,opcion,valor, resultado)
    
-
+    #-----------------------------------------------------------
+    # Funciones de...
+    #-----------------------------------------
     def __init__(self, parent):
         super().__init__(parent)        
         
-        self.label_magnitud = wx.StaticText(
-            self,
-            label="Magnitud:"
-            )
+        self.label_magnitud = wx.StaticText(self,label="Magnitud:")
 
-        self.combo_magnitud = wx.Choice(
-            self,
+        self.combo_magnitud = wx.Choice(self,
             choices=[
                 "Seleccionar...",
                 "Longitud",
@@ -105,10 +78,8 @@ class PanelUnaVariable(wx.Panel):
                 ])
 
         self.combo_magnitud.SetSelection(0)
-        
-        
-        self.textbox = wx.SpinCtrlDouble(
-           self,
+           
+        self.textbox = wx.SpinCtrlDouble(self,
             value="0.00",
             size=(120,40),
             min=-1000000,
@@ -118,189 +89,97 @@ class PanelUnaVariable(wx.Panel):
 
         self.textbox.SetDigits(2)
 
-        self.label = wx.StaticText(
-            self, 
-            label="Ingrese un valor y seleccione las unidades"
-            )
+        self.label = wx.StaticText(self,label="Ingrese un valor y seleccione las unidades")
 
-        self.label_a = wx.StaticText(
-            self,label="A")
-        
+        self.label_a = wx.StaticText(self,label="A")
 
         fuente = self.label_a.GetFont()
         fuente.SetWeight(wx.FONTWEIGHT_BOLD)
         fuente.SetPointSize(12)
+
         self.label_a.SetFont(fuente)
-
-
-        self.label_destino = wx.StaticText(
-            self,
-            label="Destino: Ninguno"
-            )
+        self.label_destino = wx.StaticText(self, label="Destino: Ninguno")
 
         self.boton1 = wx.Button(self, label='Convertir', size=(100,40))
         self.boton_limpiar = wx.Button(self, label='Limpiar', size=(100,40))
+        self.boton_volver = wx.Button(self,label="Volver",size=(100,40))
 
-        self.boton_volver = wx.Button(
-            self,
-            label="Volver",
-            size=(100,40)
-)
         self.combo_origen = wx.ComboBox(
             self,
             choices=[],
             style=wx.CB_READONLY,
-            size = (140,-1)
-            )
+            size = (140,-1))
 
         self.combo_destino = wx.ComboBox(
             self,
             choices=[],
             style=wx.CB_READONLY,
-            size = (140,-1)
-            )
+            size = (140,-1))
                     
         self.resultado = wx.StaticText(self, label="")
         self.destino = ""
         self.origen = ""
 
-
-        # SIZER 
-
+    #-----------------------------------------------------
+    # SIZER 
+    #------------
         sizer_ppal = wx.BoxSizer(wx.VERTICAL)
         fila_datos = wx.BoxSizer(wx.HORIZONTAL)
         fila_botones = wx.BoxSizer(wx.HORIZONTAL)
         fila_magnitud = wx.BoxSizer(wx.HORIZONTAL)
 
-
-        # FILA MAGNITUD 
-        fila_magnitud.Add(
-            self.label_magnitud,
-            0,
-            wx.ALL | wx.ALIGN_CENTER_VERTICAL,
-            10
-            )
-
-        fila_magnitud.Add(
-            self.combo_magnitud,
-            0,
-            wx.ALL | wx.ALIGN_CENTER_VERTICAL,
-            )
-        
+    # FILA MAGNITUD 
+        fila_magnitud.Add(self.label_magnitud, 0, wx.ALL | wx.ALIGN_CENTER_VERTICAL,10 )
+        fila_magnitud.Add(self.combo_magnitud,0, wx.ALL | wx.ALIGN_CENTER_VERTICAL,)
         
 
-        # FILA BOTONES
+    # FILA BOTONES
         fila_datos.Add(self.textbox,0,wx.ALL,10)
-
         fila_datos.AddSpacer(20)
-
         fila_datos.Add(self.combo_origen,0,wx.ALL,10)
-
         fila_datos.AddSpacer(10)
-
         fila_datos.Add(self.label_a,0,wx.ALL | wx.ALIGN_CENTER_VERTICAL,10)
-
         fila_datos.AddSpacer(10)
-
         fila_datos.Add(self.combo_destino,0,wx.ALL,10)
 
-
-        # FILA BOTONES  
-        fila_botones.Add(
-            self.boton1, 0, 
-            wx.ALL | wx.CENTER, 10)
-
-        fila_botones.Add(
-            self.boton_limpiar,
-            0,
-            wx.ALL | wx.CENTER,
-            10
-            )
+        fila_botones.Add(self.boton1, 0,wx.ALL | wx.CENTER, 10)
+        fila_botones.Add(self.boton_limpiar, 0,wx.ALL | wx.CENTER, 10)
 
         sizer_ppal.AddStretchSpacer()
-
-        sizer_ppal.Add(
-            self.label, 0,
-              wx.ALL | wx.ALIGN_CENTER_HORIZONTAL, 5
-              )
-
-        sizer_ppal.Add(
-            self.label_destino,
-            0,
-            wx.ALL | wx.ALIGN_CENTER_HORIZONTAL,
-            5
-            )
-    
-        sizer_ppal.Add(
-            fila_magnitud,
-            0,
-            wx.ALIGN_CENTER)
-
-
-
-        sizer_ppal.Add(
-         fila_datos, 0,
-           wx.ALIGN_CENTER)
-        
-        sizer_ppal.Add(
-            fila_botones,
-            0,
-            wx.ALIGN_CENTER
-        )
-        
-        sizer_ppal.Add(self.resultado,
-            0,
-            wx.ALL | wx.ALIGN_CENTER_HORIZONTAL)
-
+        sizer_ppal.Add(self.label, 0,wx.ALL | wx.ALIGN_CENTER_HORIZONTAL, 5)
+        sizer_ppal.Add(self.label_destino,0, wx.ALL | wx.ALIGN_CENTER_HORIZONTAL,5 )
+        sizer_ppal.Add(fila_magnitud,0, wx.ALIGN_CENTER)
+        sizer_ppal.Add(fila_datos, 0, wx.ALIGN_CENTER)   
+        sizer_ppal.Add( fila_botones, 0, wx.ALIGN_CENTER )
+        sizer_ppal.Add(self.resultado, 0, wx.ALL | wx.ALIGN_CENTER_HORIZONTAL)
         sizer_ppal.AddStretchSpacer()
-
-        sizer_ppal.Add(
-            self.boton_volver,
-            0,
-            wx.ALL | wx.ALIGN_RIGHT,
-            10
-)
-
+        sizer_ppal.Add(self.boton_volver, 0, wx.ALL | wx.ALIGN_RIGHT,10)
         self.SetSizer(sizer_ppal)
-        
-        # BING DE LOS BOTONE 
+            
+    # eventos de botones 
         self.boton1.Bind(wx.EVT_BUTTON, self.convertidor)
-        
         self.boton_limpiar.Bind(wx.EVT_BUTTON, self.limpiar)
-        
-        self.boton_volver.Bind(
-            wx.EVT_BUTTON,
-            self.volver
-)
+        self.boton_volver.Bind( wx.EVT_BUTTON,self.volver)
+        self.combo_origen.Bind( wx.EVT_COMBOBOX,self.seleccionar_origen )
+        self.combo_destino.Bind( wx.EVT_COMBOBOX,self.seleccionar_destino )
+        self.combo_magnitud.Bind( wx.EVT_CHOICE,  self.cambiar_magnitud)
 
-        self.combo_origen.Bind(
-            wx.EVT_COMBOBOX,
-            self.seleccionar_origen
-        )
+    #---------------------------------------------- 
 
-        self.combo_destino.Bind(
-            wx.EVT_COMBOBOX,
-            self.seleccionar_destino
-            )
-
-        self.combo_magnitud.Bind(
-            wx.EVT_CHOICE,
-            self.cambiar_magnitud
-            )
-
-    ##########
-    # EVENTOS#
-    ##########
-    def seleccionar_origen(self, event):
+    def seleccionar_origen(self, event): 
+        """
+        Captura el valor seleccionado en el combo_origen y lo almacena 
+        en el atributo de clase self.origen.
+        """           
         self.origen = self.combo_origen.GetValue()
 
-    def seleccionar_destino(self, event):
+    def seleccionar_destino(self, event): 
+        """
+        Captura el valor seleccionado en el combo_destino, lo almacena 
+        en self.destino y actualiza el texto del label_destino.
+        """             
         self.destino = self.combo_destino.GetValue()
-        self.label_destino.SetLabel(
-            f"Destino: {self.destino}"
-            )
-        
-
+        self.label_destino.SetLabel( f"Destino: {self.destino}" )
 
     def limpiar(self, event):
         self.textbox.SetValue(0.0)
@@ -311,24 +190,19 @@ class PanelUnaVariable(wx.Panel):
 
         self.combo_origen.Clear()
         self.combo_destino.Clear()
-
         self.combo_magnitud.SetSelection(0)
-
+        
         self.label_destino.SetLabel("Destino: Ninguno")
-
-        self.label.SetLabel(
-            "Seleccione una unidad de origen e ingrese un valor"
-        )
+        self.label.SetLabel("Selecciona una unidad de origen e ingresa un valor")
 
     def volver(self, event):
         ventana = self.GetParent()
         if ventana.parent:
             ventana.parent.Show()
-            
+
         ventana.Close()
 
     def cambiar_magnitud(self, event):
-        
         magnitud = self.combo_magnitud.GetStringSelection()
         
         self.combo_origen.Clear()
@@ -403,56 +277,41 @@ class PanelUnaVariable(wx.Panel):
         if self.combo_origen.GetCount() > 0:
             self.combo_origen.SetSelection(0)
             self.origen = self.combo_origen.GetValue()
+
         if self.combo_destino.GetCount() > 0:
             self.combo_destino.SetSelection(0)
             self.destino = self.combo_destino.GetValue()
 
-            self.label_destino.SetLabel(
-                f"Destino: {self.destino}"
-                )
+            self.label_destino.SetLabel( f"Destino: {self.destino}")
     
-
+#----------------------------------------------------------
+# Ventana principal de la app.
 class VentanaUnaVariable(wx.Frame):
 
     def __init__(self, parent = None):
-        super().__init__(
-            parent, 
-            title='convertidor de unidades',
-            size=(850, 550))
+        """Inicia la ventana, configura el menú y vincula los eventos."""
+        super().__init__( parent,title='convertidor de unidades',size=(850, 550))
         self.parent = parent
         self.panel = PanelUnaVariable(self)
 
+    # Configuración de la barra de menús.
         menu_bar = wx.MenuBar()
         file_menu = wx.Menu()
         self.menu_salir = file_menu.Append(wx.ID_EXIT , "salir")
         
-
-
-
         menu_historial = wx.Menu()
-
-        self.m_ver_historial = menu_historial.Append(
-            wx.ID_ANY,
-            "Ver historial"
-            )
-
+        self.m_ver_historial = menu_historial.Append(wx.ID_ANY,"Ver historial")
 
         menu_bar.Append(file_menu, "archivo")
         menu_bar.Append(menu_historial,"Historial")
         
         self.SetMenuBar(menu_bar)
         self.Bind(wx.EVT_MENU, self.salir, self.menu_salir)
-        
-        
-        # boton de historial
+    #--------------------------
+    # boton de historial
         self.Bind(wx.EVT_MENU,self.ver_historial,self.m_ver_historial)
-
-
-
         self.Bind(wx.EVT_CLOSE, self.al_cerrar)
-
         self.Show()
-
 
 
     def salir(self, event):
@@ -473,11 +332,7 @@ class VentanaUnaVariable(wx.Frame):
         if historial.strip() == "":
             historial = "No hay conversiones registradas"
 
-        wx.MessageBox(
-            historial,
-            "Historial de conversiones",
-            wx.OK | wx.ICON_INFORMATION
-            )
+        wx.MessageBox(historial,"Historial de conversiones", wx.OK | wx.ICON_INFORMATION )
 
 
 if __name__ == '__main__':
